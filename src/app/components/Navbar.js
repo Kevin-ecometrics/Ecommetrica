1620290fe47b4370fc822af7b241ac1e38536e23
Kevin-ecometrics <kevin@e-commetrics.com>
2023-10-24 11:58:14 -0700

    update hero section

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,85 +1,92 @@
+"use client";
+import React from "react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenuItem,
+  NavbarMenu,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@nextui-org/react";
 import Image from "next/image";
-import Link from "next/link";
 
-function Navbar() {
+export default function App() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    { name: "Principal", href: "#principal" },
+    { name: "Services", href: "#services" },
+    { name: "Our Consulting", href: "#consulting" },
+    { name: "Schema", href: "#schema" },
+    { name: "Success Stories", href: "#stories" },
+    { name: "Agile", href: "#agile" },
+  ];
+
+  const handleMenuItemClick = (href) => {
+    // Add your code here to handle the click event
+    console.log(`Clicked on ${href}`);
+  };
+
   return (
-    <nav className="flex flex-wrap items-center justify-between p-6 bg-transparent">
-      <div className="flex items-center flex-shrink-0 mr-6 text-white">
-        <Image
-          src="/logo.png"
-          width={50}
-          height={100}
-          alt="Logo Ecommetrica"
+    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen} className="bg-gray-100">
+      <NavbarContent className="text-black sm:hidden" justify="start">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
         />
-      </div>
-      <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 text-white border border-white rounded hover:text-gray-200 hover:border-gray-200">
-          <svg
-            className="w-3 h-3 fill-current"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
-      <div className="flex-grow block w-full lg:flex lg:items-center lg:w-auto">
-        <div className="text-sm lg:flex-grow">
-          <Link
-            href="/"
-            className="block mt-4 mr-4 text-white lg:inline-block lg:mt-0 hover:text-pink-800"
-          >
-            Principal
-          </Link>
-          <Link
-            href="/"
-            className="block mt-4 mr-4 text-white lg:inline-block lg:mt-0 hover:text-pink-800"
-          >
-            Services
-          </Link>
-          <Link
-            href="/"
-            className="block mt-4 mr-4 text-white lg:inline-block lg:mt-0 hover:text-pink-800"
-          >
-            Our Consulting
-          </Link>
-          <Link
-            href="/"
-            className="block mt-4 mr-4 text-white lg:inline-block lg:mt-0 hover:text-pink-800"
-          >
-            Schema
-          </Link>
-          <Link
-            href="/"
-            className="block mt-4 mr-4 text-white lg:inline-block lg:mt-0 hover:text-pink-800"
-          >
-            Success stories
-          </Link>
-          <Link
-            href="/"
-            className="block mt-4 mr-4 text-white lg:inline-block lg:mt-0 hover:text-pink-800"
-          >
-            Agile
-          </Link>
-        </div>
-        <div>
-          <Link
-            href="/"
-            className="block mt-4 mr-4 text-white lg:inline-block lg:mt-0 hover:text-blue-800"
-          >
-            Login
-          </Link>
-          <Link
-            href="/"
-            className="inline-block px-4 py-2 mt-4 ml-2 text-sm leading-none text-white bg-red-500 border border-white rounded hover:border-transparent hover:text-red-500 hover:bg-white lg:mt-0"
-          >
-            Contact Us
-          </Link>
-        </div>
-      </div>
-    </nav>
+      </NavbarContent>
+
+      <NavbarContent className="pr-3 sm:hidden" justify="center">
+        <NavbarBrand>
+          <Image src={"/logo.png"} width={50} height={50} alt={"logo"} />
+          <p className="font-bold text-black">Ecommetrica</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden gap-4 sm:flex" justify="center">
+        <NavbarBrand>
+          <Image src={"/logo.png"} width={50} height={50} alt={"logo"} />
+        </NavbarBrand>
+        {menuItems.map((item, index) => (
+          <NavbarItem key={`${item.name}-${index}`}>
+            <Link
+              href={item.href}
+              className="text-black hover:text-red-700"
+              onClick={() => handleMenuItemClick(item.href)}
+            >
+              {item.name}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
+
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="danger" href="#">
+            CONTACT US
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item.name}-${index}`}>
+            <Link
+              className="w-full text-red-800 hover:text-blue-700"
+              href={item.href}
+              size="lg"
+              onClick={() => handleMenuItemClick(item.href)}
+            >
+              {item.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
 }
-
-export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import HeroAnimation from './components/HeroAnimation';
-import Navbar from './components/Navbar';
+import React from "react";
+import HeroAnimation from "./components/HeroAnimation";
+import Navbar from "./components/Navbar";
 
 function Page() {
   return (
-    <div className="relative h-screen bg-gradient-to-r from-blue-800 via-purple-800 to-pink-800">
-        <Navbar />
-        <HeroAnimation />
-
-      <div className="absolute bottom-0 w-full bg-white rounded-tl-3xl h-1/4">
-      </div>
+    <div className="bg-white">
+      <Navbar />
+      <HeroAnimation />
     </div>
   );
 }
